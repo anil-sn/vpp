7557a63d86f977c3ea46578acd01d279d9b2ea64
AnilKumar Srirangapatna Nagesh <anil.nagesh@calix.com>
2025-09-04 20:37:12 +0530

    Upd

---
+++ vpp_chain/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Comprehensive VPP Chain Validation Script
+# This script tells the complete story of our VPP chain like reading a novel
+
+set -e
+sudo python3 src/main.py setup
+sudo python3 src/main.py test
